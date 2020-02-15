3080dea15b1c5a86b2e1b82c405d429ccbb44adc
vojislavsaric <vojislav.saric@gmail.com>
2020-02-15 10:43:02 +0100

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -1,6 +1,5 @@
 public class MainClass {
 	public static void main(String[] args){
-		System.out.println("Hallooo, gazdarice!");
-		System.out.println("Majstor Steva, Vi ste!");
+		System.out.println("Hallooo!?");
 	}
 }
