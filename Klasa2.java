13a97e582f762612487af34776c58cb78254b74c
vojislavsaric <vojislav.saric@gmail.com>
2020-02-21 12:10:34 +0100

    Prosirena Klasa2.java

--- Klasa2.java
+++ Klasa2.java
@@ -1,6 +1,7 @@
 
 public class Klasa2 {
 	int x;
+	double y;
 	
 	public void setX(int x) {
 		this.x = x;
@@ -8,5 +9,13 @@ public class Klasa2 {
 	
 	public int getX() {
 		return this.x;
+	}
+	
+	public void setY(double y) {
+		this.y = y;
+	}
+	
+	public double getY() {
+		return this.y;
 	}
 }
