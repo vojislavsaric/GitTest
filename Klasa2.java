a9e2311d6ada38390a7bc6b296434d7d5babd4d8
vojislavsaric <vojislav.saric@gmail.com>
2020-02-21 11:28:28 +0100

    Kreirana klasa Klasa2.java

---
+++ Klasa2.java
@@ -0,0 +1,12 @@
+
+public class Klasa2 {
+	int x;
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getX() {
+		return this.x;
+	}
+}
--- MainClass.java
+++ MainClass.java
@@ -11,9 +11,15 @@ public class MainClass {
 		//Korigovan pogresan commit i mozemo nastaviti dalje sa kodovanjem
 		novoRazvijenaMetoda();
 		
+		novoRazvijenaMetoda2();
+		
 	}
 	
 	private static void novoRazvijenaMetoda(){
 		System.out.println("Nesto novo razvijeno na develop grani...");
+	}
+	
+	private static void novoRazvijenaMetoda2(){
+		System.out.println("Metoda 2 razvijeno na develop grani...");
 	}
 }
